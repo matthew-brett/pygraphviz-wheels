8db43d2dc120d8c2283e735c0d806703b4c17555
Matthew Brett <matthew.brett@gmail.com>
2018-08-15 11:42:54 +0100

    Update pkg-config to build graphviz

--- config.sh
+++ config.sh
@@ -3,22 +3,10 @@
 FREETYPE_VERSION=2.9.1
 LIBUUID_VERSION=1.0.3
 FONTCONFIG_VERSION=2.13.0
-BZIP2_VERSION="${BZIP2_VERSION:-1.0.6}"
-# BZIP website was down when I checked on August 8 2018
-BZIP_URL=https://web.archive.org/web/20180624184806/http://bzip.org
+PKG_CONFIG_VERSIN=0.29.2
 EXPAT_TAG=50f4e16
-GRAPHVIZ_TAG=stable_release_2.40.1
+GRAPHVIZ_COMMIT=f54ac2c9  # 2.38
 GPERF_VERSION=3.1
-
-function build_bzip2 {
-    if [ -n "$IS_OSX" ]; then return; fi  # OSX has bzip2 libs already
-    if [ -e bzip2-stamp ]; then return; fi
-    fetch_unpack $BZIP_URL/${BZIP2_VERSION}/bzip2-${BZIP2_VERSION}.tar.gz
-    (cd bzip2-${BZIP2_VERSION} \
-        && make -f Makefile-libbz2_so \
-        && make install PREFIX=$BUILD_PREFIX)
-    touch bzip2-stamp
-}
 
 function build_libuuid {
     build_simple libuuid $LIBUUID_VERSION https://download.sourceforge.net/libuuid
@@ -36,13 +24,23 @@ function build_fontconfig {
 }
 
 function latest_autotools {
-    if [ -e latest-autotools-stamp ]; then
+    if [ -e ${FUNCNAME[0]}-stamp ]; then
         return
     fi
     if [ -n "$IS_OSX" ]; then
         brew upgrade autoconf automake libtool || true
     fi
-    touch latest-autotools-stamp
+    touch ${FUNCNAME[0]}-stamp
+}
+
+function upgrade_pkg_config {
+    if [ -e ${FUNCNAME[0]}-stamp ]; then
+        return
+    fi
+    if [ -z "$IS_OSX" ]; then
+        build_simple pkg-config $PKG_CONFIG_VERSION https://pkg-config.freedesktop.org/releases .tar.gz --with-internal-glib
+    fi
+    touch ${FUNCNAME[0]}-stamp
 }
 
 function build_expat {
@@ -66,12 +64,12 @@ function build_graphviz {
     build_swig
     build_freetype
     build_fontconfig
-    echo "Finished fontconfig"
     build_expat
-    echo "Finished expat"
-    # Unfortunately, this always gives the latest version
-    local out_dir=$(fetch_unpack https://graphviz.gitlab.io/pub/graphviz/stable/SOURCES/graphviz.tar.gz)
-    (cd $out_dir \
+    upgrade_pkg_config
+    git clone https://gitlab.com/graphviz/graphviz.git
+    (cd graphviz \
+        && git checkout $GRAPHVIZ_COMMIT \
+        && ./autogen.sh \
         && ./configure --prefix=$BUILD_PREFIX --enable-php=no \
         && make \
         && make install)
